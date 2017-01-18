3c7cc7033b4ebb029d871ab15b580b6d9c74e5d1
Paul <paul.jarvey@gmail.com>
2017-01-17 22:27:45 -0500

    Create fader.ino

---
+++ fader.ino
@@ -0,0 +1,18 @@
+//simple fast fade calculation, borrowed from open arduino examples
+
+int fader (int currentpixel, int faderate){
+  
+  // get the pixel value
+
+int rgb = leds.getPixel(currentpixel);
+int b1 = rgb & 255;
+int g1 = (rgb & 65280) >> 8;
+int r1 = (rgb & 16711680) >> 16;
+int b2 = b1 - faderate;
+int g2 = g1 - faderate;
+int r2 = r1 - faderate;
+int rgb2 = ((r2 << 16) | (g2 << 8) | b2);
+  
+  // 
+  return(rgb2);
+  }
