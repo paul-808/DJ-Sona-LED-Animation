0dc2eb17ae271adc98fc2dee896dc579d3f316f1
Paul <paul.jarvey@gmail.com>
2017-01-17 22:29:28 -0500

    Create xRED.ino

---
+++ xRED.ino
@@ -0,0 +1,18 @@
+// main animation for RED mode (unfinished, currently just pulses gently)
+
+void red (){
+
+              for (int c = 0; c < 256; c++){
+                int m = abs(c-128)/bright;
+                for (uint16_t v = 0;  v < 1250; v++){
+                uint8_t r = (m);
+                uint8_t g = 0;
+                uint8_t b = 0;
+                leds.setPixel(v, ((r << 16) | (g << 8) | b)); 
+                }
+                
+              leds.show();
+              }
+
+
+}
